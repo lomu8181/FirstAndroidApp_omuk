ed1d320491de75d11e57dd68856221e5b31df8d7
lomu <helomufotdevl@gmail.com>
2023-08-07 20:21:53 +0900

    Initial commit

---
+++ app/src/main/java/com/hlomu/firstandroidapp_omuk/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.hlomu.firstandroidapp_omuk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.hlomu.firstandroidapp_omuk.ui.theme.FirstAndroidApp_omukTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            FirstAndroidApp_omukTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FirstAndroidApp_omukTheme {
+        Greeting("Android")
+    }
+}
